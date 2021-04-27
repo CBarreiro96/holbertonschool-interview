4f2b1fdab50d37c3937f24b7c2c1087c103b558d
Camilo Barreiro <1845@holbertonschool.com>
2021-04-27 12:01:31 -0500

    correction betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * insert-node - Inserts a number into a sorted singly linked list.
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
  * @head: start of pointer
  * @number: new node
  * Return: address of the new element or NULL if it failed
- * */
-
+ */
 
 
 listint_t *insert_node(listint_t **head, int number)
@@ -29,23 +29,23 @@ listint_t *insert_node(listint_t **head, int number)
 		new->next = NULL;
 
 		/*Search the position the number in the list*/
-		if(*head == NULL || (*head)->n > new->n)
+		if (*head == NULL || (*head)->n > new->n)
 		{
-				/*The number is in the first position*/
-				new->next = *head;
-				*head = new;
+			/*The number is in the first position*/
+			new->next = *head;
+			*head = new;
 		}
 		else
 		{
-				/*Compare and search the postion number*/
-				while (current->next && current->next->n < new->n)
-				{
-						current = current->next;
-				}
+			/*Compare and search the postion number*/
+			while (current->next && current->next->n < new->n)
+			{
+				current = current->next;
+			}
 
-				/*Add conection of the new node with the list*/
-				new->next = current->next;
-				current->next = new;
+			/*Add conection of the new node with the list*/
+			new->next = current->next;
+			current->next = new;
 		}
 		return (new);
 	}
