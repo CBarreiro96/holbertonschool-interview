29ee11a82035ec77d50e3501765c93eac36455fc
Camilo Barreiro <1845@holbertonschool.com>
2021-05-06 11:56:16 -0500

    correct betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,20 +8,20 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    /*Structure pointer*/
+	/*Structure pointer*/
 	binary_tree_t *New_node;
 
-    /*Allocate memory*/
+	/*Allocate memory*/
 	New_node = malloc(sizeof(binary_tree_t));
-	
-    /*Validate if it is null*/
-    if (New_node == NULL)
+
+	/*Validate if it is null*/
+	if (New_node == NULL)
 		return (NULL);
-	
-    /*Add node*/
-    New_node->left = New_node->right = NULL;
-    New_node->n = value;
-    New_node->parent = parent;
+
+	/*Add node*/
+	New_node->left = New_node->right = NULL;
+	New_node->n = value;
+	New_node->parent = parent;
 
 	return (New_node);
 }
