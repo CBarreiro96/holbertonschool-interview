81dbcf2a1fd528ea9fe48a003c89143cff4992f0
Camilo Barreiro <1845@holbertonschool.com>
2021-05-12 11:16:08 -0500

    Solution exercise minmum operation

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,3 +2,27 @@
 """
 ==================== Minimum Operations ==============================
 """
+
+
+def minOperations(n):
+    """
+    How many operation we can do with number given?
+    :param n: Operation limit
+    :return: Integer
+    """
+    operations_limits = n
+
+    if type(operations_limits) is not int or operations_limits < 2:
+        return 0
+
+    min_operations = 2
+    total_operations = 0
+
+    while min_operations <= operations_limits:
+        if operations_limits % min_operations == 0:
+            total_operations += min_operations
+            operations_limits = operations_limits / min_operations
+        else:
+            min_operations += 1
+
+    return total_operations
